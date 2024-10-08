c4ebae26ea30085e8191c7e79b0d60c575883e41
Henno98 <259554@stud.inn.no>
2024-10-08 11:24:30 +0200

    Added New Project files

--- Spillmotor_Oblig_2/Oblig_2/Oblig_2/ECS.h
+++ Spillmotor_Oblig_2/Oblig_2/Oblig_2/ECS.h
@@ -1,17 +1,5 @@
 #pragma once
-#include "Dependencies/includes/glm/glm.hpp"
 class ECS
 {
-public:
-	 ECS();
-
-	 int ID;
-	 glm::vec3 Position;
-	 glm::vec3 Rotation;
-	 glm::vec3 Scale;
-	 float Mass;
-	 float HP;
-	 float Attack;
-	 float Defense;
 };
 
